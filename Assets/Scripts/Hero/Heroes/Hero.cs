6eda1e1361ca752005176dc4edde2ae18bf412f6
kkjs0517 <kkjs0517@gmail.com>
2026-08-10 18:41:45 +0900

    asset : 영웅 공격 vfx 적용
    스프라이트 받아와서 이펙트 재생하는 EffectPlayer 생성 및 적용
    영웅 공격 기능 HeroAttack으로 분리

--- Assets/Scripts/Hero/Heroes/Hero.cs
+++ Assets/Scripts/Hero/Heroes/Hero.cs
@@ -14,8 +14,8 @@ public abstract class Hero : MonoBehaviour, IDamageable
     [SerializeField] private float heroCriChance;
     [SerializeField] private bool isDead;
     [SerializeField] private float hitRadius;
+    [SerializeField] private float attackTime;
 
-    private float heroCriDamage = 2f;
     private HeroLocationEnum location;
     // private HeroAttackTypeEnum attackType;
     private int heroMaxLv = 3;
@@ -23,16 +23,12 @@ public abstract class Hero : MonoBehaviour, IDamageable
 
     [SerializeField] private TMP_Text name_T;
 
-    [SerializeField] private float attackTime;
-    private float attackTimer;
-
     [SerializeField] private Transform heroRoot;
     [SerializeField] private float moveSpeed = 8f;
     [SerializeField] private LayerMask enemyLayer;
     private GameObject targetEnemy;
     private bool isMoving;
-    private bool canAttack;
-    private bool isAttacking;
+    
     private float searchRange = 50f;
     private float meleeRange = 1.3f;
     
@@ -40,6 +36,7 @@ public abstract class Hero : MonoBehaviour, IDamageable
     protected HeroStat stat;
     protected HeroStateEnum heroState;
     private HeroEquipmentManager heroEquip;
+    private HeroAttack attack;
 
     public int HeroID => heroID;
     public string HeroName => heroName;
@@ -98,22 +95,18 @@ public abstract class Hero : MonoBehaviour, IDamageable
         isDead = false;
         this.location = location;
 
-        attackTimer = attackTime;
-        isAttacking = false;
-
         if (name_T != null) name_T.text = heroName;
 
         heroState = HeroStateEnum.Idle;
         heroEquip = GetComponent<HeroEquipmentManager>();
+        attack = GetComponent<HeroAttack>();
     }
 
-    private void Update()
+    public void Update()
     {
-        attackTimer += Time.deltaTime;
-
         if (targetEnemy == null) SearchEnemy();
         if (targetEnemy != null && location == HeroLocationEnum.Front) MoveToEnemy();
-        if (targetEnemy != null && location == HeroLocationEnum.Back) Attack(targetEnemy);
+        if (targetEnemy != null && location == HeroLocationEnum.Back) attack.Attack(targetEnemy);
 
         ChangeState();
     }
@@ -160,13 +153,13 @@ public abstract class Hero : MonoBehaviour, IDamageable
 
         Vector2 direction = targetEnemy.transform.position - transform.position;
 
-        if (direction.sqrMagnitude <= meleeRange * meleeRange) canAttack = true;
-        else canAttack = false;
+        if (direction.sqrMagnitude <= meleeRange * meleeRange) attack.ChangeCanAttack(true);
+        else attack.ChangeCanAttack(false);
 
-        if (canAttack)
+        if (attack.CanAttack)
         {
             isMoving = false;
-            Attack(targetEnemy);
+            attack.Attack(targetEnemy);
         }
         else
         {
@@ -189,28 +182,6 @@ public abstract class Hero : MonoBehaviour, IDamageable
         heroRoot.localScale = scale;
     }
 
-    protected virtual void Attack(GameObject enemy)
-    {
-        float criRan = Random.Range(1f, 100f);
-        float damage = heroAtk; // 적 방어력 적용
-
-        if (attackTimer >= attackTime && !isAttacking)
-        {
-            isAttacking = true;
-            attackTimer = 0f;
-
-            if (criRan <= heroCriChance) damage *= heroCriDamage;
-
-            // 공격 적용, 치명타 적용
-            Debug.Log(gameObject.name + "의 공격, 피해량 : " + damage);
-        }
-    }
-
-    public void AttackStop()
-    {
-        heroState = HeroStateEnum.Idle;
-        isAttacking = false;
-    }
 
     protected virtual void Die()
     {
@@ -218,7 +189,7 @@ public abstract class Hero : MonoBehaviour, IDamageable
     }
 
     // 비용(매개변수) 지불 추가
-    private void LvUp()
+    public void LvUp()
     {
         if (heroLv >= heroMaxLv) return;
 
@@ -239,8 +210,14 @@ public abstract class Hero : MonoBehaviour, IDamageable
     {
         if (isDead) heroState = HeroStateEnum.Die;
         else if (isMoving) heroState = HeroStateEnum.Move;
-        else if (isAttacking) heroState = HeroStateEnum.Attack;
+        else if (attack.IsAttacking) heroState = HeroStateEnum.Attack;
         else heroState = HeroStateEnum.Idle;
+    }
+
+    public void AttackStop()
+    {
+        heroState = HeroStateEnum.Idle;
+        attack.StopIsAttacking();
     }
 
     private void OnDrawGizmos()
@@ -249,10 +226,6 @@ public abstract class Hero : MonoBehaviour, IDamageable
         Gizmos.color = Color.red;
         if (location == HeroLocationEnum.Front) Gizmos.DrawWireSphere(
             new Vector3(transform.position.x, transform.position.y, transform.position.z), meleeRange);
-
-        // 적 탐색 사거리
-        // Gizmos.color = Color.orange;
-        // Gizmos.DrawWireSphere(new Vector3(transform.position.x, transform.position.y, transform.position.z), searchRange);
     }
 
     public void EquipStatApply(Equipment equip)
