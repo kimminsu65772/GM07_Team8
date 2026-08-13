03490e249a0132a37fa93bf1c13320d71aa0b4c2
sym43 <remin0430@gmail.com>
2026-08-13 15:52:05 +0900

    feat/ 업그레이드 관련 값들을 ui에 넘겨주는 기능 구현
    업그레이드 관련 값 ui에서 가져갈 수있게 함.

--- Assets/Scripts/Airship/Upgrade/AirshipUpgradeController.cs
+++ Assets/Scripts/Airship/Upgrade/AirshipUpgradeController.cs
@@ -6,6 +6,7 @@ public class AirshipUpgradeController : MonoBehaviour
     // 아직 재화를 안정하고, 레벨당 요구량 테이블도 없어서 일단 임시로 해둠.
     [SerializeField] private CurrencyType upgradeCurrency = CurrencyType.Gold;
     [SerializeField, Min(0)] private int upgradeCost = 100;
+    [SerializeField] private AirshipStatTable statTable;
 
     // 일단 스탯이 0인 상태를 생성
     // LoadState에서 세이브 값으로 갱신된다.
@@ -56,8 +57,15 @@ public class AirshipUpgradeController : MonoBehaviour
             return false;
         }
 
+        int currentLevel = upgradeState.GetLevel(statType);
         // -1인 스탯, 이속 공속은 업그레이드 대상이 아니므로 혹시 모를 방지는 해둠.
-        if (upgradeState.GetLevel(statType) < 0)
+        if (currentLevel < 0)
+        {
+            return false;
+        }
+
+        // 이미 최대 레벨이면 막음
+        if (currentLevel >= statTable.GetMaxLevel(statType))
         {
             return false;
         }
@@ -75,6 +83,7 @@ public class AirshipUpgradeController : MonoBehaviour
         return true;
     }
 
+    // TODO 코스트를 어떻게 할지 정하면 데이터 테이블과 세부로직 정하기.
     private bool TrySpendUpgradeCost()
     {
         return PlayerInfo.Instance.TrySpendCurrency(
@@ -95,9 +104,73 @@ public class AirshipUpgradeController : MonoBehaviour
         OnUpgradeChanged?.Invoke(upgradeState);
     }
 
-    // ui쪽에서 enum을 기반으로 레벨을 가져갈때를 상정함.
-    public int GetLevel(AirshipStatType statType)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+    #region UI 관련 함수들
+
+    public bool IsMaxLevel(AirshipStatType statType)
+    {
+        return upgradeState.GetLevel(statType) >= statTable.GetMaxLevel(statType);
+    }
+
+    public int GetCurrentLevel(AirshipStatType statType)
     {
         return upgradeState.GetLevel(statType);
     }
+
+    public float GetCurrentStat(AirshipStatType statType)
+    {
+        return statTable.GetStatValue(statType, GetCurrentLevel(statType));
+    }
+
+    // ui 원초적으론 ui에서 ismaxlevel이 true일땐 이게 호출되면 안되게 설계해야함.
+    // 다만 혹시 모를 상황을 대비해 방지해둠.
+    public int GetNextLevel(AirshipStatType statType)
+    {
+        if (IsMaxLevel(statType))
+            return -1;
+        return GetCurrentLevel(statType) + 1;
+    }
+
+    public float GetNextStat(AirshipStatType statType)
+    {
+        int nextLevel = GetNextLevel(statType);
+
+        if (nextLevel < 0)
+        {
+            return -1f;
+        }
+
+        return statTable.GetStatValue(statType, nextLevel);
+    }
+
+    // TODO 코스트를 어떻게 할지 정하면 데이터 테이블과 세부로직 정하기.
+    public int GetCost(AirshipStatType statType)
+    {
+        return upgradeCost;
+    }
+
+    #endregion
 }
