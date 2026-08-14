5216129277f3bcfcbd62cae0fdc2b902945a9932
kkjs0517 <kkjs0517@gmail.com>
2026-08-14 14:19:08 +0900

    prefab : 영웅 체력바 구현

--- Assets/Scripts/Hero/Heroes/HeroHPBar.cs
+++ Assets/Scripts/Hero/Heroes/HeroHPBar.cs
@@ -1,14 +1,19 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HeroHPBar : MonoBehaviour
 {
-    void Start()
+    [SerializeField] private Slider hpBar;
+    private Hero hero;
+
+    private void Awake()
     {
-        
+        hero = GetComponent<Hero>();
+        hpBar.interactable = false;
     }
 
     void Update()
     {
-        
+        hpBar.value = hero.HeroCurrentHP / hero.HeroMaxHP;
     }
 }
