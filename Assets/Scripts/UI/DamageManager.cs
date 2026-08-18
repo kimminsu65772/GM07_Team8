041c6b98ac4bf8f045ce899b1dbfc2a19697b1aa
hoy467 <hoyhoy4678@gmail.com>
2026-08-18 16:24:48 +0900

    assest:add
    비행선 장비 연결했습니다.

--- Assets/Scripts/UI/DamageManager.cs
+++ Assets/Scripts/UI/DamageManager.cs
@@ -11,18 +11,17 @@ public class DamageManager : MonoBehaviour
         if (Instance == null) Instance = this;
         else Destroy(gameObject);
     }
-    public void ShowDamage(int damage, Vector3 position)
+    public void ShowDamage(DamageInfo damageInfo, Vector3 position)
     {
         if (damagePopupPrefab == null) return;
 
-        Vector3 spawnPos = position + new Vector3(Random.Range(-0.3f, 0.3f), Random.Range(0.5f, 1f), 0);
-
-        GameObject popup = Instantiate(damagePopupPrefab, spawnPos, Quaternion.identity, worldCanvasTransform);
+        Vector3 spawnPos = position + new Vector3(Random.Range(-0.3f, 0.3f),Random.Range(0.5f, 1f), 0);
+        GameObject popup = Instantiate(damagePopupPrefab,spawnPos,Quaternion.identity,worldCanvasTransform);
         DamagePopup popupScript = popup.GetComponent<DamagePopup>();
 
         if (popupScript != null)
         {
-            popupScript.Setup(damage);
+            popupScript.Setup(damageInfo);
         }
     }
 }
