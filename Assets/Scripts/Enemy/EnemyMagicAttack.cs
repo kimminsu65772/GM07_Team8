cede5fe4b5f462813199a391d6d6accb320295a1
YeoHaeng-J <gozldwmftq2@gmail.com>
2026-08-24 17:51:39 +0900

    feat:기본 적 4종 프리팹 및 마법 공격 구현
    - 근거리 적 2종과 원거리 적 2종 프리팹 추가
    - 적 종류별 EnemyData 생성 및 적용
    - 적별 Animator Override Controller 적용
    - 궁수 공격 애니메이션 및 원거리 공격 설정
    - 마법사 공격 애니메이션과 타깃 위치 폭발 공격 구현
    - 폭발 이펙트 프리팹 적용
    - EnemyTargetSelector에 마법 공격 타깃 연동
    - 애니메이션 이벤트 수신 처리 보완

---
+++ Assets/Scripts/Enemy/EnemyMagicAttack.cs
@@ -0,0 +1,105 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+[RequireComponent(typeof(EnemyStats))]
+public class EnemyMagicAttack : MonoBehaviour
+{
+    [Header("Explosion")]
+    [SerializeField] private GameObject explosionEffectPrefab;
+    [SerializeField] private float explosionEffectDuration = 1f;
+    [SerializeField] private float castDelay = 0.3f;
+    [SerializeField] private float explosionYOffset = 0.5f;
+
+    private Transform target;
+    private IDamageable targetDamageable;
+    private EnemyStats enemyStats;
+    private Coroutine castCoroutine;
+
+    private void Awake()
+    {
+        enemyStats = GetComponent<EnemyStats>();
+    }
+
+    private void OnDisable()
+    {
+        if (castCoroutine != null)
+        {
+            StopCoroutine(castCoroutine);
+            castCoroutine = null;
+        }
+    }
+
+    public void SetTarget(Transform newTarget)
+    {
+        target = newTarget;
+        targetDamageable = target != null ? target.GetComponentInParent<IDamageable>() : null;
+    }
+
+    public void BeginCast()
+    {
+        if (!isActiveAndEnabled || enemyStats == null || enemyStats.IsDead)
+        {
+            return;
+        }
+
+        if (castCoroutine != null)
+        {
+            StopCoroutine(castCoroutine);
+        }
+
+        castCoroutine = StartCoroutine(CastAfterDelay());
+    }
+
+    private IEnumerator CastAfterDelay()
+    {
+        yield return new WaitForSeconds(castDelay);
+        castCoroutine = null;
+        CastExplosion();
+    }
+
+    public void CastExplosion()
+    {
+        if (target == null || targetDamageable == null || enemyStats == null || enemyStats.IsDead)
+        {
+            return;
+        }
+
+        Vector3 explosionPosition = target.position + Vector3.up * explosionYOffset;
+
+        if (explosionEffectPrefab != null)
+        {
+            GameObject explosionEffect = Instantiate(explosionEffectPrefab, explosionPosition, Quaternion.identity);
+            Canvas explosionCanvas = explosionEffect.GetComponent<Canvas>();
+            RectTransform explosionRect = explosionEffect.GetComponent<RectTransform>();
+
+            if (explosionCanvas != null)
+            {
+                explosionCanvas.renderMode = RenderMode.WorldSpace;
+                explosionCanvas.overrideSorting = true;
+                SortingGroup targetSortingGroup = target.GetComponentInParent<SortingGroup>();
+                if (targetSortingGroup != null)
+                {
+                    explosionCanvas.sortingLayerID = targetSortingGroup.sortingLayerID;
+                    explosionCanvas.sortingOrder = targetSortingGroup.sortingOrder + 10;
+                }
+                else
+                {
+                    explosionCanvas.sortingLayerName = "Default";
+                    explosionCanvas.sortingOrder = 100;
+                }
+            }
+
+            if (explosionRect != null)
+            {
+                explosionRect.position = explosionPosition;
+                explosionRect.sizeDelta = new Vector2(64f, 64f);
+                explosionRect.localScale = Vector3.one * 0.02f;
+            }
+
+            Destroy(explosionEffect, explosionEffectDuration);
+        }
+
+        targetDamageable.TakeDamage(new DamageInfo(enemyStats.AttackPower));
+    }
+}
